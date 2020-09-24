a5d2bac578e9a9869fd9eebd09d5b570badb41b4
Shubh Agarwal <sazorinos1111@gmail.com>
2020-09-24 11:14:57 +0530

    some programs on for loop

---
+++ p5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+/*some experiments with looping functions*/
+int i=5;
+for (;i;i--)
+//actuall we can write any valid statement inside the foe
+printf("%d\n",i);
+
+return 0;
+}
---
+++ p6.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int i;
+    for (i=0;i++<10;)
+    printf("%d\n",i);
+// in this program test counter itself is behaving like test counter
+    return 0;
+}
