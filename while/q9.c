a5419356b7b49cfa3c0bf69ce857e5be3caf04d5
Shubh Agarwal <sazorinos1111@gmail.com>
2020-10-03 20:07:03 +0530

    All question of while section of let-us-C are added to git

--- while/q9.c
+++ while/q9.c
@@ -1,10 +1,23 @@
 #include<stdio.h>
 
 int main(){
-  int max,min,i,imp;
-  char choice;
-  
-  choice='n';
-  
-  
+  int max=-32768,min=32767,i,a;
+  char choice='y';
+  while(choice=='y'){
+    printf("Enter a number:");
+    scanf("%d",&a);
+    //i forgot why i assigned name 'imp' to variable now i am changing variable name to 'a'
+    if(max<a)
+      max = a;
+    if(min>a)
+      min = a;
+    printf("Do you want to enter another number:");
+    fflush(stdin);
+    scanf("%s",&choice);
+  }
+  printf("Maximum value added by you is:%d\n",max);
+  printf("Minimum value added by you is:%d\n",min);
+  printf("Range of the set of values added by you is %d\n",max-min);
+
+  return 0;
 }
